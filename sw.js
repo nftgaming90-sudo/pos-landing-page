233e9e7cec788013d8dba8ed22cc73dd743f33cc
Aan <nftgaming90@gmail.com>
2026-04-05 11:44:45 +0800

    fitur pwa

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'poskita-v1';
+const ASSETS_TO_CACHE = [
+  'index.html',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/sql.js/1.8.0/sql-wasm.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/sql.js/1.8.0/sql-wasm.wasm'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Strategi: Coba ambil dari Network dulu, kalau gagal (offline) ambil dari Cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
